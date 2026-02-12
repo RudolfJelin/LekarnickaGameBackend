275c84e771bd8e11f1b1e921c91baa216fe02434
Líbal, Rudolf <libalrud@fel.cvut.cz>
2026-02-12 23:28:34 +0100

    prototype done

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function copy(object) {
 function load_first_aid_items(){
 
     // load and split into lines
-    const string = fs.readFileSync('./lekarnicka_test.txt', 'utf8').split(/\r?\n/)
+    const string = fs.readFileSync('./lekarnicka.txt', 'utf8').split(/\r?\n/)
 
     // console.log("string", string);
 
@@ -84,7 +84,19 @@ function load_first_aid_items(){
     // trim results
     result = result.map(s => s.trim());
 
+    shuffleArray(result);
+
     return result;
+}
+
+// https://stackoverflow.com/a/12646864
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
 
 function previous_phase(phase){
